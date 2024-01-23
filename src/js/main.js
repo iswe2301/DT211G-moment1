91ec4d5c86d88d7281d7d47e2da152c0581e80e9
Isa Westling <isawestling.blogg@hotmail.com>
2024-01-23 15:22:33 +0100

    lagt till funktioner för att hämta data med fetch-api och läsa ut till dom

--- src/js/main.js
+++ src/js/main.js
@@ -29,3 +29,30 @@ function dropDownMenu() {
         mobilenavEl.style.display = "block";
     }
 }
+
+// Asynkron funktion som hämtar data med fetch-api. Try och catch för att underlätta felahntering.
+async function getData() {
+    try {
+        const response = await fetch("https://dahlgren.miun.se/ramschema_ht23.php");
+        const data = await response.json();
+        return data;
+        // Fångar upp felmeddelande
+    } catch (error) {
+        console.error("Error-meddelande:", error);
+        throw error;
+    }
+}
+
+// Asynkron funktion för att visa data
+async function displayData() {
+    const data = await getData(); // Inväntar att data har hämtats från getData-funktionen
+    // Hämtar ID för tabell
+    const tableEl = document.getElementById("course-table");
+    // Loopar igenom varje data och skriver ut till DOM
+    data.forEach(data => {
+        tableEl.innerHTML += `<tr><td>${data.code}</td><td>${data.coursename}</td><td>${data.progression}</td></tr>`;
+    })
+}
+
+displayData();
+
